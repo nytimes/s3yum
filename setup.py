d20b48d00353ae62676eeb296d9c47c6161d2aa9
Canaday <203349@Andrew-Canaday-5865.local>
2013-07-26 11:15:00 -0400

    Added setup.py for installs and moved s3yum into /bin.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup, Extension
+
+setup(name = 's3yum',
+       version = '1.0',
+       description = 'Command line utility for managing yum repos in S3',
+       author = 'Andrew Canaday',
+       author_email = 'andrew.canaday@nytimes.com',
+       scripts = ['bin/s3yum'])
+
+# EOF
+
